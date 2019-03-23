4b52ccd25e18af49d3247e3fcb3f62dc070e490e
keeley <keeley.jun@qq.com>
2019-03-23 20:06:18 +0800

    ryrty

--- consumer-feign/src/main/java/cc/sitec/consumer/FeignApplication.java
+++ consumer-feign/src/main/java/cc/sitec/consumer/FeignApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class FeignApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FeignApplication.class,args);
+        SpringApplication.run(FeignApplication.class, args);
     }
 }
